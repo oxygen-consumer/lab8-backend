7e7727b7012753d99c807e1905f6b09ad28f87f1
Zaharie Stefan <stefan@zaha.tech>
2021-11-22 17:58:52 +0200

    Try to fix angular test server

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "api-lab8.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "api-lab8.herokuapp.com", "localhost:4200"]
 
 
 # Application definition
