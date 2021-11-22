09df66190a405dfa088e1fbddff995446669a0ea
Zaharie Stefan <stefan@zaha.tech>
2021-11-22 17:53:25 +0200

    Add localhost to CORS

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "api-lab8.herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "api-lab8.herokuapp.com", "localhost"]
 
 
 # Application definition
